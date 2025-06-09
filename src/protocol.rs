f03f191cab5acab68883a11c01e0b741ab55330f
the_a <the_alchemist@fastmail.com>
2025-06-09 16:54:21 -0400

    Work on basic structs

--- src/protocol.rs
+++ src/protocol.rs
@@ -6,7 +6,7 @@ use crate::{copy_le, parse_le};
 pub(crate) const MAGIC: u8 = 0xEC;
 pub(crate) const VERSION: u8 = 1;
 pub(crate) const TLV_HEADER_SIZE: usize = 6;
-pub(crate) const PL_HEADER_SIZE: usize = 12;
+pub(crate) const PL_HEADER_SIZE: usize = 12; // Verified from ESP docs
 pub(crate) const CRC_LEN: usize = 2;
 
 /// Compute CRCs; for by the one embedded in the payload header, and at the end of the message.
@@ -64,6 +64,7 @@ pub(crate) enum PacketTypeHci{
 
 #[derive(Clone, Copy, PartialEq, TryFromPrimitive)]
 #[repr(u8)]
+/// Private
 pub(crate) enum PacketTypePriv{
     A = 0,
     B = 1, // todo
@@ -91,36 +92,41 @@ pub(crate) enum PacketType {
 }
 
 impl PacketType {
-    pub fn val(&self) -> u8 {
+    pub fn to_byte(&self) -> u8 {
         match self {
             Self::Hci(p) => *p as u8,
             Self::Priv(p) => *p as u8,
         }
     }
+
+    pub fn from_byte(val: u8) -> Self {
+        // todo temp; fix.
+       Self::Priv(PacketTypePriv::A)
+    }
 }
 
 #[derive(Clone, Copy, PartialEq, TryFromPrimitive)]
 #[repr(u8)]
-/// Interface type.
+/// Interface type. See ESP-Hosted-MCU readme, section 7.2
+/// Embassy shows different Implied to be the same in ESP doc, but not explicitly. Esp proto:
 pub(crate) enum IfType {
-    // todo: How to serialize?
     Invalid = 0,
-    Sta = 1,
+    Stat = 1,
     Ap = 2,
     Serial = 3,
     Hci = 4,
     Priv = 5,
     Test = 6,
     Eth = 7,
-    Max = 8,
+    Max = 8
 
-    // todo: Embassy net uses:
-    //     Sta = 0,
-    //     Ap = 1,
-    //     Serial = 2,
-    //     Hci = 3,
-    //     Priv = 4,
-    //     Test = 5,
+    // Embassy:
+    // Sta = 0,
+    // Ap = 1,
+    // Serial = 2,
+    // Hci = 3,
+    // Priv = 4,
+    // Test = 5,
 }
 
 #[derive(Clone, Copy, PartialEq, TryFromPrimitive)]
@@ -168,18 +174,28 @@ pub(crate) fn slip_encode(src: &[u8], out: &mut [u8]) -> usize {
 
 /// Adapted from `esp-hosted-mcu/common/esp_hosted_header.h`
 /// This is at the start of the message, and is followed by the RPC header.
+/// See ESP-hosted-MCU readme, section 7.1.
+///
+/// todo: This checks with both embassy, and the WIP Espressif doc.
 struct PayloadHeader {
+    /// Interface type
     pub if_type: IfType, // 2 4-bit values
+    /// Interface number
     pub if_num: u8,      // 2 4-bit values
     pub flags: u8,
+    /// Payload length
     pub len: u16,
+    /// Offset to payload
     pub offset: u16,
+    /// Header + payload checksum
     pub checksum: u16,
+    /// Sequence number for tracking packets (Useful in debugging)
     pub seq_num: u16,
-    pub throttle_cmd: u8, // 2 4-bit values
-    // u8 reserved; `reserve2:6`
+    /// Flow control
+    pub throttle_cmd: u8, // First two bits of this byte.
+    // u8 reserved; `reserve2:6`; same byte as `throttle_cmd`
 
-    // todo: Hmm. This may not be right. Maybe it should be HCI or PRIV only?
+    // From Esp doc: First 3 bits may be reserved. The remaining bits for HCI or PRivate packet type?
     pub pkt_type: PacketType,
 }
 
@@ -211,11 +227,10 @@ impl PayloadHeader {
         copy_le!(buf, self.seq_num, 8..10);
 
         // byte 10:  [ reserved2:6 | throttle_cmd:2 ]
-        // buf[10] = (self.throttle_cmd & 0x03)
-        //     | ((self.reserved2 & 0x3F) << 2);
+        buf[10] = self.throttle_cmd; // todo: QC if you need a shift.
 
         // byte 11: union field
-        buf[11] = self.pkt_type.val();
+        buf[11] = self.pkt_type.to_byte();
 
         buf
     }
@@ -231,12 +246,8 @@ impl PayloadHeader {
         let checksum = parse_le!(buf, u16, 6..8);
         let seq_num = parse_le!(buf, u16, 8..10);
 
-        let throttle_cmd = buf[10] & 0x03;
-        let reserved2 = (buf[10] >> 2) & 0x3F;
-
-        // todo temp; fix.
-        // let pkt_type = buf[11].try_into().unwrap_or(PacketType::Priv(PacketTypePriv::A));
-        let pkt_type = PacketType::Priv(PacketTypePriv::A);
+        let throttle_cmd = buf[10] & 3;
+        let pkt_type = PacketType::from_byte(buf[11]);
 
         Self {
             if_type,
@@ -252,50 +263,107 @@ impl PayloadHeader {
     }
 }
 
-/// This header is followed by a payload of 0-1024 bytes. (Transport agnostic). then
-/// a CRC-16/CCITT-false. Polynomial: 0x1021. Init: 0xffff. The CRC is calculated over
-/// everything except the magic byte. (Version through end of payload.
-///
-/// This is preceded by a Payload leader.
+
+// /// Type-length-value header. See host/drivers/virtual_serial_if/serial_if.c
+// struct TlvHeader {
+//     pub magic: u8,   // Always 0xEC.
+//     pub version: u8, // Always 1, for now.
+//     /// Payload length, not including CRC. LE.
+//     pub length: u16,
+//     pub module: Module,
+//     pub command: Command,
+// }
+
+// impl TlvHeader {
+//     pub fn new(module: Module, command: Command, payload_len: usize) -> Self {
+//         Self {
+//             magic: MAGIC,
+//             version: VERSION,
+//             length: payload_len as u16,
+//             module,
+//             command,
+//         }
+//     }
+//
+//     pub fn to_bytes(&self) -> [u8; TLV_HEADER_SIZE] {
+//         let length = self.length.to_le_bytes();
+//         [
+//             self.magic,
+//             self.version,
+//             length[0],
+//             length[1],
+//             self.module as u8,
+//             self.command as u8,
+//         ]
+//     }
+//
+//     pub fn from_bytes(buf: &[u8]) -> Self {
+//         Self {
+//             magic: buf[0],
+//             version: buf[1],
+//             length: parse_le!(buf, u16, 2..4),
+//             module: Module::try_from_primitive(buf[4]).unwrap(),
+//             command: Command::try_from_primitive(buf[5]).unwrap(),
+//         }
+//     }
+// }
+
+#[derive(Clone, Copy, PartialEq, TryFromPrimitive, Default)]
+#[repr(u8)]
+/// Type-length-value header. See host/drivers/virtual_serial_if/serial_if.c
+pub(crate) enum TlvType {
+    #[default]
+    EndpointName = 0x01,
+    Data = 0x02,
+}
+
+// #[derive(Clone, Copy, PartialEq, TryFromPrimitive)]
+// #[repr(u8)]
+// // todo
+// pub(crate) enum DataType {
+//     A = 0
+// }
+
+/// Type-length-value header. See host/drivers/virtual_serial_if/serial_if.c
 struct TlvHeader {
-    pub magic: u8,   // Always 0xEC.
-    pub version: u8, // Always 1, for now.
-    /// Payload length, not including CRC. LE.
-    pub length: u16,
-    pub module: Module,
-    pub command: Command,
+    pub endpoint_type: TlvType,
+    pub endpoint_length: u16,
+    // pub endpoint_value: &[u8], // endpoint length
+    pub data_type: TlvType,
+    pub data_length: u16,
+    // pub data_value: &[u8], // data length
 }
 
 impl TlvHeader {
-    pub fn new(module: Module, command: Command, payload_len: usize) -> Self {
-        Self {
-            magic: MAGIC,
-            version: VERSION,
-            length: payload_len as u16,
-            module,
-            command,
-        }
-    }
+    pub fn to_bytes(&self, buf: &mut [u8], endpoint_value: &[u8], data_value: &[u8])  {
+        buf[0] = self.endpoint_type as u8;
+        copy_le!(buf, self.endpoint_length, 1..3);
 
-    pub fn to_bytes(&self) -> [u8; TLV_HEADER_SIZE] {
-        let length = self.length.to_le_bytes();
-        [
-            self.magic,
-            self.version,
-            length[0],
-            length[1],
-            self.module as u8,
-            self.command as u8,
-        ]
+        let mut i = 3;
+        buf[i..i + self.endpoint_length as usize].copy_from_slice(endpoint_value);
+        i += self.endpoint_length as usize;
+
+        buf[i] = self.data_type as u8;
+        i += 1;
+
+        copy_le!(buf, self.data_length, i..i + 2);
+        i += 2;
+
+        buf[i..i + self.data_length as usize].copy_from_slice(data_value);
     }
 
     pub fn from_bytes(buf: &[u8]) -> Self {
+        // todo: Where do we read the values?
+        // todo: Return a Result for failed enum parsing?
+
+        let endpoint_length = parse_le!(buf, u16, 1..3);
+        let epl = endpoint_length as usize;
+
         Self {
-            magic: buf[0],
-            version: buf[1],
-            length: parse_le!(buf, u16, 2..4),
-            module: Module::try_from_primitive(buf[4]).unwrap(),
-            command: Command::try_from_primitive(buf[5]).unwrap(),
+            endpoint_type: buf[0].try_into().unwrap_or_default(),
+            endpoint_length,
+            data_type: buf[3 + epl].try_into().unwrap_or_default(),
+            data_length: parse_le!(buf, u16, 3 + epl..3 + epl + 2),
         }
     }
 }
